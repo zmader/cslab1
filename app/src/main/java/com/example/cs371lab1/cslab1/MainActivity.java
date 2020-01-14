33e3b367f5d9b3e10f08b9e9f3008a60e0e1f46d
zmader <zmader@gmail.com>
2020-01-13 19:50:10 -0800

    initial commit

---
+++ app/src/main/java/com/example/cs371lab1/cslab1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs371lab1.cslab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
